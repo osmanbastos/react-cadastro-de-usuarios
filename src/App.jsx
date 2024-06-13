6bb63e744edde7b81db4513abc64f0c7fd37cc95
osmanbastos <osmanbastos@hotmail.com>
2024-06-13 02:23:49 -0300

    fix: import UserForm removed from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import React, { useState } from 'react';
-import UserForm from './components/UserForm';
 import Logo from './components/templates/logo/Logo.jsx';
 import Nav from './components/templates/nav/Nav.jsx';
 import Main from './components/templates/main/Main.jsx';
